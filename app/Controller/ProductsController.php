c7a2e472d2eede54cd4942f45ec6fe8d2003cd84
Pietro Brum Costabile <pcostabile@credipronto.com.br>
2022-03-02 18:43:31 -0300

    Fixing add

--- app/Controller/ProductsController.php
+++ app/Controller/ProductsController.php
@@ -14,12 +14,11 @@ class ProductsController extends AppController {
 
     public function add() {
         if ($this->request->is('post')) {
-            $this->Product->create();
+            $this->request->data['Product']['user_responsable'] = $this->Auth->user('id'); // Adicionada essa linha
+            //echo '<pre>'; print_r($this->request->data);exit();
             if ($this->Product->save($this->request->data)) {
-                $this->Flash->success(__('The Product has been saved'));
+                $this->Flash->success('Your product has been saved.');
                 $this->redirect(array('action' => 'index'));
-            } else {
-                $this->Flash->error(__('The Product could not be saved. Please, try again.'));
             }
         }
     }
@@ -46,6 +45,11 @@ class ProductsController extends AppController {
             $this->Flash->success('The product with id: ' . $id . ' has been deleted.');
             $this->redirect(array('action' => 'index'));
         }
+    }
+
+    public function isAuthorized($user) {
+        return true;
+        
     }
 
 }
--- app/View/Products/add.ctp
+++ app/View/Products/add.ctp
@@ -5,7 +5,7 @@
         <legend><?php echo __('Add Product'); ?></legend>
         <?php echo $this->Form->input('name');
         echo $this->Form->input('price');
-        echo $this->Form->input('quantiy');
+        echo $this->Form->input('quantity');
         echo $this->Form->input('status', array(
             'options' => array('new' => 'New', 'used' => 'Used')
         ));
